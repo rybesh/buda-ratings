4829987f0615b8318a41323e4e51af9e95f9d4b7
Shane Bussmann <sbussmann@gmail.com>
2016-05-22 15:07:35 -0400

    BudaRating class is functional.

--- Code/scrape_buda.py
+++ Code/scrape_buda.py
@@ -40,7 +40,8 @@ experience rating
 class BudaRating(object):
 
     def __init__(self):
-        pass
+        self.scrape_leagues()
+        self.define_ratings()
 
     def scrape_leagues(self):
 
@@ -86,7 +87,7 @@ class BudaRating(object):
 
         # define base ratings by division (arbitrarily assigned based on my
         # experience)
-        div_ratings = {'summer club': {'4/3 Div 1': 1800,
+        div_ratings = {'Summer Club': {'4/3 Div 1': 1800,
                                        '4/3 Div 2': 1400,
                                        '4/3 Div 3': 1000,
                                        '4/3 Div 4': 900,
@@ -96,7 +97,7 @@ class BudaRating(object):
                                        '5/2 Div 4': 800,
                                        'Open Div 1': 1400,
                                        'Open Div 2': 1200},
-                       'fall club': {'4/3 Div 1': 1700,
+                       'Fall Club': {'4/3 Div 1': 1700,
                                      '4/3 Div 2': 1300,
                                      '4/3 Div 3': 900,
                                      '4/3 Div 4': 800,
@@ -123,26 +124,44 @@ class BudaRating(object):
         # dictionary specifying the rating that each team is associated with
         team_rating = {}
 
-        # plan is to build a dataframe with all teams in the BUDA database
+        # plan is to build a dataframe with a row for all teams in the BUDA
+        # database.  columns will be teamid, season, league type, year,
+        # division name, average plus/minus per game.  other methods will use
+        # these columns to produce new columns.  the main ones i'm thinking
+        # about now are: observed rating, predicted rating.  observed rating
+        # is a base rating associated with division name plus a modifier that
+        # is related to average plus/minus per game.  predicted rating is
+        # based on the player ratings for all members of the team.  to
+        # estimate base rating for hat league divisions, i'll need to inspect
+        #  the predicted ratings for every team.  then i can groupby division
+        #  name and look at mean and standard deviations for each
+        # division.  maybe break it down by year and see if there are trends
+        # with time.
         allteamids = []
+        allteamnames = []
         allseasons = []
         alltypes = []
         allyears = []
-        alldivs = []
-        allratings = []
+        alldivnames = []
+        alldivratings = []
+        allplusminus = []
 
         # loop over all the leagues in league_meta
         for leagueid in self.league_meta.index:
 
             league_season = self.league_meta.ix[leagueid, 'season']
             league_type = self.league_meta.ix[leagueid, 'type']
+            league_year = self.league_meta.ix[leagueid, 'year']
             if league_season == 'Winter':
                 league_type = 'Hat'
-            league_meta = [league_season, league_type].join(' ')
+            league_meta = " ".join([league_season, league_type])
 
             # only analyze leagues where league_type is Hat or Club
-            if league_type != 'Hat' or league_type != 'Club':
-                continue
+            if league_type != 'Hat':
+                if league_type != 'Club':
+                    print("{} is not Hat or Club, skipping {}".format(
+                        league_type, leagueid))
+                    continue
 
             # scrape the scores for this league
             scheme = 'http'
@@ -188,7 +207,7 @@ class BudaRating(object):
                 continue
 
             # associate the division names with this league id
-            self.league_meta.ix[leagueid, 'divnames'] = divnames
+            # self.league_meta.ix[leagueid, 'divnames'] = divnames
 
             # get the appropriate rating dictionary
             if league_meta in self.div_ratings:
@@ -200,6 +219,7 @@ class BudaRating(object):
 
             # define base ratings by division (arbitrarily assigned based on my experience)
             dfdata['div'] = np.zeros(len(dfdata))
+            dfdata['divname'] = np.zeros(len(dfdata))
             for i in range(len(divnames)-1):
                 try:
                     divstart = np.where(dfdata['Team'] == divnames[i])[0][0]
@@ -213,11 +233,13 @@ class BudaRating(object):
                     continue
                 try:
                     dfdata.ix[divstart + 1: divend, 'div'] = div_ratings[divnames[i]]
+                    dfdata.ix[divstart + 1: divend, 'divname'] = divnames[i]
                 except KeyError:
                     print("No base rating for {}, skipping league {}".format(divnames[i], leagueid))
                     continue
             try:
                 dfdata.ix[divend + 1:, 'div'] = div_ratings[divnames[-1]]
+                dfdata.ix[divend + 1:, 'divname'] = divnames[-1]
             except KeyError:
                 print("No base rating for {}, skipping league {}".format(divnames[-1], leagueid))
                 continue
@@ -261,7 +283,11 @@ class BudaRating(object):
             # link the team rating to each player on that team
             for teamid, teamname in zip(teamids, teamnames):
                 try:
-                    adhocrating = dfdata.ix[dfdata['Team'] == teamname.strip(' '), 'rating'].values[0]
+                    index = dfdata['Team'] == teamname.strip(' ')
+                    adhocrating = dfdata.ix[index, 'adhocrating'].values[0]
+                    divrating = dfdata.ix[index, 'div'].values[0]
+                    divisionname = dfdata.ix[index, 'divname'].values[0]
+                    avgplusminus = dfdata.ix[index, 'avgplusminus'].values[0]
                 except IndexError:
                     print("Couldn't match {} to scores database, skipping this team.".format(teamname))
                     continue
@@ -294,12 +320,41 @@ class BudaRating(object):
                         player_teams[player].append(teamid)
                     else:
                         player_teams[player] = [teamid]
+
+                # build the super list of teams
+                allteamids.append(teamid)
+                allteamnames.append(teamname)
+                allseasons.append(league_season)
+                alltypes.append(league_type)
+                allyears.append(league_year)
+                alldivnames.append(divisionname)
+                alldivratings.append(divrating)
+                allplusminus.append(avgplusminus)
+
             print("Finished successfully with league {}".format(leagueid))
+
+        alldf = pd.DataFrame({'teamid': allteamids,
+                              'teamname': allteamnames,
+                              'season': allseasons,
+                              'type': alltypes,
+                              'year': allyears,
+                              'divname': alldivnames,
+                              'divrating': alldivratings,
+                              'plusminus': allplusminus})
 
         self.player_teams = player_teams
         self.team_players = team_players
         self.team_rating = team_rating
         self.league_teams = league_teams
+        self.allteams = alldf
+
+    def observed_rating(self):
+
+        base_rating = self.allteams['divrating']
+        normalizer = 60.
+        plusminus = self.allteams['plusminus']
+        observed_ratings = base_rating + normalizer * plusminus
+        self.allteams['observed_ratings'] = observed_ratings
 
     def dump_buda(self, prefix):
         f = open(prefix + '_player_teams.p', 'wb')
@@ -311,27 +366,33 @@ class BudaRating(object):
         f = open(prefix + '_league_teams.p', 'wb')
         pickle.dump(self.league_teams, f)
 
+    def write_allteams(self, prefix):
+        self.allteams.to_csv(prefix + '_allteams.csv', index=False)
+
+    def load_allteams(self, prefix):
+        self.allteams = pd.read_csv(prefix + '_allteams.csv')
+
     def load_buda(self, prefix):
         f = open(prefix + '_player_teams.p', 'r')
-        pickle.load(self.player_teams, f)
+        self.player_teams = pickle.load(f)
         f = open(prefix + '_team_players.p', 'r')
-        pickle.load(self.team_players, f)
+        self.team_players = pickle.load(f)
         f = open(prefix + '_team_rating.p', 'r')
-        pickle.load(self.team_rating, f)
+        self.team_rating = pickle.load(f)
         f = open(prefix + '_league_teams.p', 'r')
-        pickle.load(self.league_teams, f)
+        self.league_teams = pickle.load(f)
 
     def predict_team(self, team_id):
 
         """
 
         :param team_id: id of the team for which to predict a rating
-        :return: dictionary with predicted ratings of players on team_id
+        :return: list of predicted ratings of players on team_id
 
         """
 
-        # instantiate the dictionary of predicted ratings for this team
-        team_ratings = {}
+        # instantiate the list of predicted ratings for this team
+        team_ratings = []
 
         # get the list of players for this team
         players = self.team_players[team_id]
@@ -339,7 +400,7 @@ class BudaRating(object):
         # for each player, get their rating based on previous performance
         for player in players:
             teams = self.player_teams[player]
-            teams = np.array(teams).astype('float')
+            teams = np.array(teams).astype('int')
 
             # list of previous teams for this player
             previous_teams_index = teams < float(team_id)
@@ -349,10 +410,10 @@ class BudaRating(object):
             # very good
             # TODO: use a google search for this player somehow
             if len(previous_teams) == 0:
-                team_ratings[player] = 800
+                team_ratings.append(800)
             else:
-                previous_ratings = [self.team_rating[team_key] for team_key in
-                                    previous_teams]
+                previous_ratings = [self.team_rating[str(team_key)] for
+                                    team_key in previous_teams]
 
                 # if there was no div rating set, then the rating will be
                 # centered on zero and should not be used in previous_ratings
@@ -363,149 +424,18 @@ class BudaRating(object):
                 # might want to refactor this line, since there are many
                 # possible ways to generate a single rating for a given player
                 if previous_ratings[okratings].size > 0:
-                    team_ratings[player] = np.mean(previous_ratings[okratings])
+                    team_ratings.append(np.mean(previous_ratings[okratings]))
                 else:
                     # this player doesn't have any club experience on record,
                     # so default their rating to 800
                     # TODO: define div ratings for hat leagues
-                    team_ratings[player] = 800
+                    team_ratings.append(800)
 
-        return team_ratings
+        return np.mean(team_ratings)
 
-    def predict_div(self, league_id, divname):
+    def predicted_rating(self):
 
-        # instantiate dictionary to store ratings for teams in this league
-        div_ratings = {}
+        prating = [self.predict_team(str(i)) for i in self.allteams['teamid']]
 
-        self.league_meta.ix[league_id, 'divnames']
-
-        # get the list of teams in this league / division
-        team_ids = self.league_teams[league_id]
-        for team_id in team_ids:
-            team_ratings = self.predict_team(team_id)
-            div_ratings[team_id] = np.mean(team_ratings)
-
-        return div_ratings
-
-
-
-
-# extract the league id for this league
-springhat2016id = '40258'
-leagueid = springhat2016id
-
-# scrape the list of teams for this league
-teamsurl = 'http://www.buda.org/hatleagues/rosters.php?section=showTeams&league=' + leagueid
-response = urllib2.urlopen(teamsurl)
-teams_soup = BeautifulSoup(response)
-
-# generate list of team ids and names for this league
-tdlist = teams_soup.find_all('td', class_='infobody')
-teamids = []
-teamnames = []
-for td in tdlist:
-    try:
-        url = td.a['href']
-        idindex = url.index('team=')
-        whichindex = url.index('which=')
-        teamids.append(url[idindex+5:whichindex-1])
-        teamnames.append(td.a.get_text())
-    except:
-        continue
-
-# find all players associated with each team
-teamratings = {}
-for teamid, teamname in zip(teamids, teamnames):
-
-    teamurl = 'http://www.buda.org/hatleagues/rosters.php?section=showTeamRoster&team=' + teamid
-    response = urllib2.urlopen(teamurl)
-    roster_soup = BeautifulSoup(response)
-
-    playerratings = []
-    players = [td.get_text() for td in roster_soup.find_all("td", class_="infobody")]
-    for player in players:
-        if player in all_players:
-            playerratings.append(players_means[player])
-        else:
-            # if someone hasn't played club league, they probably aren't very good
-            playerratings.append(800)
-    # the team rating is the average of the player ratings for that team
-    teamratings[teamname] = np.mean(playerratings)
-print("Finished successfully with league {}".format(leagueid))
-
-
-# In[156]:
-
-sns.distplot(pd.DataFrame(teamratings.values()).dropna(), kde=False, bins=10)
-plt.axvline(teamratings['Team 20 (20)'], label='Team 20')
-plt.legend(loc='auto')
-plt.ylabel('Number of Teams')
-plt.xlabel('Team Rating')
-plt.savefig('Team20Rating.png')
-
-
-# In[161]:
-
-teamratings['Team 27 (27)'] = 1000
-
-
-# In[162]:
-
-keylist = []
-valuelist = []
-for key in teamratings.keys():
-    keylist.append(key)
-    valuelist.append(teamratings[key])
-
-
-# In[163]:
-
-shl = pd.DataFrame({'team':keylist, 'rating':valuelist})
-
-
-# In[164]:
-
-shl = shl.sort('rating', ascending=False)
-
-
-# In[166]:
-
-shl.team
-
-
-# In[180]:
-
-5/25.
-
-
-# In[179]:
-
-2/28.
-
-
-# In[ ]:
-
-def (rating1, rating2):
-    
-    # tune k so that rating differential of ... corresponds to point ratio of ...
-    # 800 ... 0.5
-    # 400 ... 0.15
-    # 200 ... 0.07
-    # 100 ... 0.035
-    
-    point_ratio1 = 1 / (1 + np.exp(-k * x))
-    
-    return point_ratio1
-
-
-# In[181]:
-
-indices = [-1200, -800,-400,-200,-100,0,100,200,400,800, 1200]
-outputs = [-1, -0.5, -0.2, -0.07, -0.03, 0.0, 0.03, 0.07, 0.2, 0.5, 1]
-plt.plot(outputs, indices,'-o')
-
-
-# In[ ]:
-
-
+        self.allteams['predicted_rating'] = prating
 
